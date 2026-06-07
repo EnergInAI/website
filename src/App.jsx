c1a3435a1657a2cff3a4a2ced72ba2f699b4ff3d
Brajesh <brajeshs2004@gmail.com>
2026-06-07 23:08:16 +0530

    updated tagline

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="bg-black h-screen flex items-center justify-center">
       <h1 className="text-red-500 text-4xl font-bold">
-        Tailwind Working Hai 🔥
+         
       </h1>
     </div>
   );
